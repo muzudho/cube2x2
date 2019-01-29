314535889c7341e1f788054a1332528fb2eb1a43
むずでょ <muzudho1@gmail.com>
2019-01-29 21:01:49 +0900

    とりあえず。

---
+++ cube2x2/DevelopmentUserControl.cs
@@ -0,0 +1,18 @@
+﻿namespace Cube2x2
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// 展開図コントロール。
+    /// </summary>
+    public partial class DevelopmentUserControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DevelopmentUserControl"/> class.
+        /// </summary>
+        public DevelopmentUserControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ cube2x2/Form1.cs
@@ -0,0 +1,26 @@
+﻿namespace Cube2x2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// 2x2のキューブ。
+    /// </summary>
+    public partial class Cube2x2 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cube2x2"/> class.
+        /// </summary>
+        public Cube2x2()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
